e3b2dab3bdd46cee8bede5554cb5bbaccfe880d9
Kevin Keen <35079676+thekeendesigns@users.noreply.github.com>
2018-07-03 15:24:35 -0400

    'new navbar and mackenzie content addition'

---
+++ js/nav-animation.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var scrollTop = 0;
+    $(window).scroll(function(){
+      scrollTop = $(window).scrollTop();
+       $('.counter').html(scrollTop);
+      
+      if (scrollTop >= 100) {
+        $('#global-nav').addClass('scrolled-nav');
+      } else if (scrollTop < 100) {
+        $('#global-nav').removeClass('scrolled-nav');
+      } 
+      
+    });  
+  });
