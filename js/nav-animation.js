30b97d7be55e63fa65980d0a2fd6b72e79348b00
Kevin Keen <35079676+thekeendesigns@users.noreply.github.com>
2018-12-22 20:02:00 -0500

    changed homepage

--- js/nav-animation.js
+++ js/nav-animation.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
-    var scrollTop = 0;
+    
+  var scrollTop = 0;
+
     $(window).scroll(function(){
       scrollTop = $(window).scrollTop();
        $('.counter').html(scrollTop);
       
       if (scrollTop >= 100) {
-        $('#global-nav').addClass('scrolled-nav');
+        $('.global-nav').addClass('-collapse');
       } else if (scrollTop < 100) {
-        $('#global-nav').removeClass('scrolled-nav');
-      } 
-      
+        $('.global-nav').removeClass('-collapse');
+      }    
     });  
   });
+
