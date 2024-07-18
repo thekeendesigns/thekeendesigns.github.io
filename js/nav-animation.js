70400e6ade14c69c943bb742f3e4dd6a3cc4c4f2
Kevin Keen <35079676+thekeendesigns@users.noreply.github.com>
2024-07-18 03:11:18 -0400

    added workNavigation color toggle

--- js/nav-animation.js
+++ js/nav-animation.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
     
-  var scrollTop = 0;
+var scrollTop = 0;
 
-    $(window).scroll(function(){
-      scrollTop = $(window).scrollTop();
-       $('.counter').html(scrollTop);
-      
-      if (scrollTop >= 100) {
-        $('.global-nav').addClass('-collapse');
-      } else if (scrollTop < 100) {
-        $('.global-nav').removeClass('-collapse');
-      }    
-    });  
-  });
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+      $('.counter').html(scrollTop);
+    
+    if (scrollTop >= 100) {
+      $('.global-nav').addClass('-collapse');
+    } else if (scrollTop < 100) {
+      $('.global-nav').removeClass('-collapse');
+    }    
+  });  
+});
 
+// work Navigation
+  $('#workNavigation li').on('click', function(){
+    $('#workNavigation li').removeClass('workSelected');
+    $(this).addClass('workSelected');
+});
