78086a43622c5a926b4e2672953b2d90db63588e
Kevin Keen <35079676+thekeendesigns@users.noreply.github.com>
2024-07-25 18:38:28 -0400

    updated nav animation

--- js/nav-animation.js
+++ js/nav-animation.js
@@ -1,21 +1,20 @@
-$(document).ready(function(){
-    
-var scrollTop = 0;
-
-  $(window).scroll(function(){
-    scrollTop = $(window).scrollTop();
-      $('.counter').html(scrollTop);
-    
-    if (scrollTop >= 100) {
-      $('.global-nav').addClass('-collapse');
-    } else if (scrollTop < 100) {
-      $('.global-nav').removeClass('-collapse');
-    }    
-  });  
-});
 
 // work Navigation
   $('#workNavigation li').on('click', function(){
     $('#workNavigation li').removeClass('workSelected');
     $(this).addClass('workSelected');
 });
+
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navBarContainer").style.top = "2%";
+    document.getElementById("navBarContainer").style.transition = "all 0.3s ease";
+  } else {
+    document.getElementById("navBarContainer").style.top = "-70px";
+    document.getElementById("navBarContainer").style.transition = "all 0.3s ease";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
