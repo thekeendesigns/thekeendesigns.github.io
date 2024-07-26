7da6e52738c8bc6a45cc55953e212049d3094a96
Kevin Keen <35079676+thekeendesigns@users.noreply.github.com>
2024-07-25 22:47:10 -0400

    updated for mobile

--- js/nav-animation.js
+++ js/nav-animation.js
@@ -11,9 +11,13 @@ var currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navBarContainer").style.top = "2%";
     document.getElementById("navBarContainer").style.transition = "all 0.3s ease";
+    document.getElementById("navBarMobContainer").style.top = "1%";
+    document.getElementById("navBarMobContainer").style.transition = "all 0.3s ease";
   } else {
     document.getElementById("navBarContainer").style.top = "-70px";
     document.getElementById("navBarContainer").style.transition = "all 0.3s ease";
+    document.getElementById("navBarMobContainer").style.top = "-70px";
+    document.getElementById("navBarMobContainer").style.transition = "all 0.3s ease";
   }
   prevScrollpos = currentScrollPos;
 }
